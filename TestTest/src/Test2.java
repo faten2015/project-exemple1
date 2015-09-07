a8c908fd101ddf8f3fabd058a873dae2f517f4d6
stagiaire <stagiaire@Stagiaire02>
2015-09-07 10:30:06 +0200

    commit2

--- TestTest/src/Test2.java
+++ TestTest/src/Test2.java
@@ -1,4 +1,22 @@
 
 public class Test2 {
 
+	String str;
+	int a;
+	public String getStr() {
+		return str;
+	}
+	public void setStr(String str) {
+		this.str = str;
+	}
+	public int getA() {
+		return a;
+	}
+	public void setA(int a) {
+		this.a = a;
+	}
+	
+	
+	
+	
 }
